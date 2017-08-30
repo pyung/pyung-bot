b13f00c09f15c908cbb121f948f6b605586b3861
Olamilekan <lwahab@terragonltd.com>
2017-08-30 08:22:17 +0100

    Dependency update

--- config/extensions.py
+++ config/extensions.py
@@ -4,11 +4,7 @@ from logging.handlers import RotatingFileHandler
 
 from flask_restful import Api
 from flask_wtf.csrf import CSRFProtect
-from tinymongo import TinyMongoClient
-
-connection = TinyMongoClient()
 
 csrf_protect = CSRFProtect()
 api = Api()
 handler = RotatingFileHandler('log.log', maxBytes=10000, backupCount=1)
-db = connection.MoodBot.user_collection
